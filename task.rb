fe5311089a861753f506e407d02424fe2c3aae64
KURASAWA Nozomu <nabetaro@caldron.jp>
2018-06-10 18:13:58 +0900

    add notice task

---
+++ task.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+$LOAD_PATH.push File.dirname( __FILE__ ) + '/lib'
+
+require 'line_client'
+
+message = <<EOS
+定期連絡です。以下の練習の出欠に入力をお願いします。
+https://chouseisan.com/s?h=0aa8981788814f73b92f7cd784624961
+EOS
+
+#return unless Time.now.monday?
+client = LineClient.new
+client.push(message)
