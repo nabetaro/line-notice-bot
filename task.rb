9185e6c5f93fa7ec9d3b6886cba6b33988948ba6
KURASAWA Nozomu <nabetaro@caldron.jp>
2018-06-12 05:45:25 +0900

    execute wday from ARGV

--- task.rb
+++ task.rb
@@ -3,12 +3,14 @@ $LOAD_PATH.push File.dirname( __FILE__ ) + '/lib'
 
 require 'line_client'
 
+exec_wday = ARGV[0].to_i
+
 message = <<EOS
-定期連絡です。以下の練習の出欠に入力をお願いします。
+定期連絡です。練習の出欠に入力をお願いします。
 https://chouseisan.com/s?h=0aa8981788814f73b92f7cd784624961
 EOS
 
 now = Time.now
-return unless now.monday?
+return unless now.wday == exec_wday
 client = LineClient.new
 client.push(message)
