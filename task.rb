b67f98e761af958594d78aecedf6617bcc6c8505
KURASAWA Nozomu <nabetaro@caldron.jp>
2018-06-12 05:51:24 +0900

    multiple wdays

--- task.rb
+++ task.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.push File.dirname( __FILE__ ) + '/lib'
 
 require 'line_client'
 
-exec_wday = ARGV[0].to_i
+exec_wdays = ARGV.map{|v| v.to_i}
 
 message = <<EOS
 定期連絡です。練習の出欠に入力をお願いします。
@@ -11,6 +11,6 @@ https://chouseisan.com/s?h=0aa8981788814f73b92f7cd784624961
 EOS
 
 now = Time.now
-return unless now.wday == exec_wday
+return unless exec_wdays.include?(now.wday)
 client = LineClient.new
 client.push(message)
